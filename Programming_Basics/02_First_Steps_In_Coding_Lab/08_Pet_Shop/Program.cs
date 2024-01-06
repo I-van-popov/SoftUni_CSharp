943a6e911b82ae6e60b5bb84c22147f0ad088b7d
I-van-popov <149477968+I-van-popov@users.noreply.github.com>
2024-01-06 23:37:14 +0200

    Added Projects

---
+++ Programming_Basics/02_First_Steps_In_Coding_Lab/08_Pet_Shop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _08_Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = 2.5;
+            double catFoodPrice = 4.0;
+
+            double totalSum = (dogFood * dogFoodPrice) + (catFoodPrice * catFood);
+            Console.WriteLine($"{totalSum} lv.");
+        }
+    }
+}
---
+++ Programming_Basics/02_First_Steps_In_Coding_Lab/09_Yard_Greening/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _09_Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double yard = double.Parse(Console.ReadLine());
+
+            double pricePerOneM = 7.61;
+            double price = yard * pricePerOneM;
+
+            double discount = price * 0.18;
+
+            double totalPrice = price - discount;
+
+            Console.WriteLine($"The final price is: {totalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
